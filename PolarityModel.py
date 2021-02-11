aa27777a93394302948203f99b0517303ea1f404
ppkgtmm <pkgtm1998@gmail.com>
2021-02-11 23:03:37 +0700

    edit model

--- PolarityModel.py
+++ PolarityModel.py
@@ -175,8 +175,8 @@ class PolarityModel:
         batches = [32]
         dim = [200, 300]
         x = np.expand_dims(data['text'], -1)
-        y = data['target']
-        x, _x, y, _y = self.split_data(x,y, train_size=0.7)
+        y = np.expand_dims(data['target'], -1)
+        x, _x, y, _y = self.split_data(x, y, train_size=0.7)
         param_grid = dict(
             optimizer=optimizers,
             batch_size=batches,
