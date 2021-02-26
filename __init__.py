d919e08bc0aff14c1569a88e59ec35927a0075b0
ppkgtmm <pkgtm1998@gmail.com>
2021-02-27 02:02:49 +0700

    added utils functions

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from utils import OH_fit, OH_fit_transform, OH_transform, \
-    read_data, split_data, get_optimizer, get_tuner, \
-    dump, load, get_callbacks, get_model_from_config, \
-    get_sequences, model_evaluate 
